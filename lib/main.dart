8ef5e2ffc89d9ad7d9fdf22a3769dd803fcd50b9
lPatricio_VargasR <1722110158@utectulancingo.edu.mx>
2023-11-06 08:59:39 -0600

    UPDATE VALIDATOR ISBN

--- lib/main.dart
+++ lib/main.dart
@@ -203,7 +203,7 @@ class _HomePageState extends State<HomePage> {
                 labelText: 'ISBN',
               ),
               validator: (value) {
-                if (value!.isEmpty || value.length < 10 || !RegExp(r'^[0-9]+$').hasMatch(value)) {
+                if (value!.isEmpty || !RegExp(r'^[0-9]+$').hasMatch(value)) {
                   return "Ingresa un ISBN correcto";
                 }
                 return null;
